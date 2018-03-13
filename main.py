e20b799aa8e7b6107b2d22b50f72837c30191170
Katadoka <katadoka@gmail.com>
2018-03-13 16:59:44 +0200

    Issue 2

--- main.py
+++ main.py
@@ -7,23 +7,30 @@ from bs4 import BeautifulSoup
 BASE_URL = 'https://medium.com/search?q=Python'
 
 
-def search_links(links):
+def get_article(link):
+    responses = requests.get(link)
+    soup = BeautifulSoup(responses.content, 'html.parser')
+    information_one_headers = {'title': '', 'text': '', 'tags': []}
+    information_one_headers['title'] = soup.select('.graf--title')[0].get_text()
+    for link in soup.select('div.section-inner'):
+        information_one_headers['text'] += link.get_text()
+    for link in soup.select('ul.tags a.link'):
+        information_one_headers['tags'].append(link.get_text())
+    return information_one_headers
+
+
+def search_links():
+    response = requests.get(BASE_URL)
+    sp = BeautifulSoup(response.content, 'html.parser')
     information = []
-    for links in sp.select('div.postArticle-content a[data-action=open-post]'):
-        a = (links.get('href'))
-        responses = requests.get(a)
-        soup = BeautifulSoup(responses.content, 'html.parser')
-        for links in soup.select('div.section-inner'):
-            information.append(links.get_text())
-        for links in soup.select('ul.tags a.link'):
-            information.append(links.get_text())
-            information.append(links.get('href'))
+    for link in sp.select('div.postArticle-content a[data-action=open-post]'):
+        information.append(get_article(link.get('href')))
 
     with open('Information.txt', 'w') as f_out:
         json.dump(information, f_out)
 
-response = requests.get(BASE_URL)
+    with open('Information.txt', 'r') as f_in:
+        a1 = json.load(f_in)
+        print([a['title'] for a in a1])
 
-sp = BeautifulSoup(response.content, 'html.parser')
-
-search_links(sp)
+search_links()
