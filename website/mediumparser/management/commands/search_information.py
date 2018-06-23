caf4b6cc9089c3d639692606c103ff02ea073d4b
Katadoka <katadoka@gmail.com>
2018-06-23 19:32:15 +0300

    update admin

--- website/mediumparser/management/commands/search_information.py
+++ website/mediumparser/management/commands/search_information.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from mediumparser.models import Tags, Store
+from mediumparser.models import Tag, Article
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,37 +7,37 @@ from bs4 import BeautifulSoup
 class Command(BaseCommand):
 
     BASE_URL = 'https://medium.com/search?q='
-    BASE_ID = 1
 
     @staticmethod
-    def get_article_db(link, url, tags_id):
+    def store_article(link, url):
         text = ""
         responses = requests.get(link)
         soup = BeautifulSoup(responses.content, 'html.parser')
         title = soup.select('.graf--title')[0].get_text()
         for link in soup.select('div.section-inner'):
             text += link.get_text()
+        article, _ = Article.objects.get_or_create(url=url, title=title, text=text)
         for link in soup.select('ul.tags a.link'):
             tag = link.get_text()
-            Tags.objects.get_or_create(tags_id=tags_id,tags=tag)
-        Store.objects.get_or_create(url=url, title=title, text=text, tags_id=tags_id)
+            tag, _ = Tag.objects.get_or_create(name=tag)
+            article.tags.add(tag)
 
     @staticmethod 
-    def search_links_db(key, tags_id):
+    def search_links_db(key):
         response = requests.get(Command.BASE_URL + key)
         sp = BeautifulSoup(response.content, 'html.parser')
         for link in sp.select('div.postArticle-content a[data-action=open-post]'):
             url = link.get('href')
-            Command.get_article_db(link.get('href'), url, tags_id)
-            tags_id += 1
+            Command.store_article(link.get('href'), url)
+
 
     @staticmethod 
-    def seach_content(keys, tags_id):
+    def seach_content(keys):
         for key in keys:
-            Command.search_links_db(key, tags_id)
-            tags_id += 10
+            Command.search_links_db(key)
+
 
     def handle(self, *args, **options):
         with open("keys.txt") as file:
             keys = [row.strip() for row in file]
-        Command.seach_content(keys, Command.BASE_ID)
+        Command.seach_content(keys)
--- website/mediumparser/models.py
+++ website/mediumparser/models.py
@@ -1,12 +1,26 @@
 from django.db import models
-
-class Tags(models.Model):
-    tags_id = models.IntegerField()
-    tags = models.CharField(max_length=30)
+from django.utils.html import format_html
+from django.template.defaultfilters import truncatechars
 
 
-class Store(models.Model):
-    url = models.CharField(max_length=50)
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    url = models.URLField(max_length=50)
     title = models.CharField(max_length=30)
     text = models.TextField()
-    tags = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+    def show_url(self):
+        return format_html("<a href='{url}'>{url}</a>", url=self.url)
+    show_url.short_description = 'url'
+
+    def short_text(self):
+        return truncatechars(self.text, 100)
+    short_text.short_description = 'text'
+
